13c5ab51f775bf6093d6e2af0aec971b32adcda0
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-11-13 07:22:02 +0900

    update import file path

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Record from './components/Record';
-import History from './components/History';
-import Setting from './components/Setting';
-import Login from './components/Login';
-import Signup from './components/Signup';
+import Record from './view/Record';
+import History from './view/History';
+import Setting from './view/Setting';
+import Login from './view/Login';
+import Signup from './view/Signup';
 
 Vue.use(Router);
 
